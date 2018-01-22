2498fa484d87fbfb309d0040a47145f47baa80a1
Emily Wells Jimenez and Rane Fields <emily+rane@>
2018-01-22 15:21:35 -0800

    Add adding contacts

---
+++ ApiWithMvc/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ApiWithMvc.Models;
+
+namespace ApiWithMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Contact> model = Contact.GetContacts();
+            return View(model);
+        }
+
+        public IActionResult AddContactForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddContactForm(Contact newContact)
+        {
+            string validationCode = newContact.GetValidation();
+            return Content(validationCode);
+        }
+
+        public IActionResult GetMessages()
+        {
+            var allMessages = Message.GetMessages();
+            return View(allMessages);
+        }
+
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(Message newMessage)
+        {
+            newMessage.Send();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ApiWithMvc/Models/Contact.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace ApiWithMvc.Models
+{
+    public class Contact
+    {
+        public string Sid { get; set; }
+        public string Phone_Number { get; set; }
+        public string Friendly_Name { get; set; }
+
+        public string GetValidation()
+        {
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            var request = new RestRequest($"Accounts/{EnvironmentVariables.AccountSID}/OutgoingCallerIds.json", Method.POST);
+            client.Authenticator = new HttpBasicAuthenticator(EnvironmentVariables.AccountSID, EnvironmentVariables.AuthToken);
+            request.AddParameter("PhoneNumber", Phone_Number);
+            request.AddParameter("FriendlyName", Friendly_Name);
+            var response = new RestResponse();
+
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+			JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+
+            string result = JsonConvert.DeserializeObject<string>(jsonResponse["validation_code"].ToString());
+            return result;
+        }
+
+        public static List<Contact> GetContacts()
+        {
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            var request = new RestRequest($"Accounts/{EnvironmentVariables.AccountSID}/OutgoingCallerIds.json", Method.GET);
+            client.Authenticator = new HttpBasicAuthenticator(EnvironmentVariables.AccountSID, EnvironmentVariables.AuthToken);
+            var response = new RestResponse();
+
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            List<Contact> contactList = JsonConvert.DeserializeObject<List<Contact>>(jsonResponse["outgoing_caller_ids"].ToString());
+            return contactList;
+        }
+
+        private static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
